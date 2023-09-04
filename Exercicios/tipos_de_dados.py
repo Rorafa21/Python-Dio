30d2988e402d14e77a2bd9edf11be492765667d9
Rodrigo Rafa <rodrigorafa71@gmail.com>
2023-09-04 16:58:46 -0300

    TIPOS_DE_DADOS
    Signed-off-by: Rodrigo Rafa <rodrigorafa71@gmail.com>

---
+++ Exercicios/tipos_de_dados.py
@@ -0,0 +1,20 @@
+#Tipos de dados no Python
+#Dentre eles estão o tipo STRING/INT/BOOL/FLOAT dentre outros.
+#INT
+print("Tipo Inteiro:")
+print(1+1)
+print("-------------")
+#float
+print("Tipo Float:")
+print(1.60 + 2.40)
+print("-------------\n")
+
+#bool
+print("Tipo Booleano:")
+print(True)
+print(False)
+print("-------------\n")
+#string 
+print("Tipo String:")
+print("Ola mundo")
+print("-------------\n")
