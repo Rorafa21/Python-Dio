dfa11c50ce841819077b4b5ebe94a1727d425004
Rodrigo Rafa <rodrigorafa71@gmail.com>
2023-09-04 20:26:03 -0300

    Variaveis
    Signed-off-by: Rodrigo Rafa <rodrigorafa71@gmail.com>

---
+++ Exercicios/variaveis.py
@@ -0,0 +1,21 @@
+# !IMPORTANTE!
+# No python não precisamos definir necessariamente o tipo da variavel
+
+nome = "Rodrigo"
+idade = 22
+
+nome = "Rafaelly"
+print(nome, idade)
+
+
+VALORANT_ELO = [
+    "ferro"
+    "bronze"
+    "prata"
+    "gold"
+    "platina"
+    "Diamante"
+    "Ascendente"
+    "Imortal"
+    "Radiante"
+]
