d6c14e9a7089c36bf06b70c7813cb84d09855299
Rodrigo Rafa <rodrigorafa71@gmail.com>
2023-09-20 10:30:13 -0300

    if saque
    Signed-off-by: Rodrigo Rafa <rodrigorafa71@gmail.com>

--- Exemplos/repeticao.py
+++ Exemplos/repeticao.py
@@ -1,3 +1,8 @@
 saldo = 1000
 opcao = int(input("Selecione a opção desejada.\nOpção [1] Saque\nOpção [2] Extrato\n"))
 
+if opcao == 1:
+    print("Opções de notas de R$10 R$20 R$50 R$100")
+    saque = float(input("Informe o Valor que deseja sacar: "))
+    saldo -= saque
+    print(saldo)
