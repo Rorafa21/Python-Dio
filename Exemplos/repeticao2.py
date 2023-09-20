29f79fac081e43785df0c4fb76ec93db879c90fd
Rodrigo Rafa <rodrigorafa71@gmail.com>
2023-09-20 10:41:26 -0300

    if_maior_complemento
    Signed-off-by: Rodrigo Rafa <rodrigorafa71@gmail.com>

--- Exemplos/repeticao2.py
+++ Exemplos/repeticao2.py
@@ -6,4 +6,11 @@ if idade >= 18:
     input("Seu cpf para cadastro:")
     input("Digite seu email: ")
     pagamento = int(input("Forma de pagamento da CNH: [1]Débito [2]Crédito [3]Boleto\n"))
-    
+    if pagamento == 1:
+        print("Certo será débito!")
+    elif pagamento == 2:
+        print("Certo será crédito!")
+    elif pagamento == 3:
+        print("Certo sera emitido o boleto e enviado por email.")
+    else:
+        sys.exit("Opção invalida")
