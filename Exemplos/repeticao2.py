5463f2365a06c337256d5c39d2519be1d158bfb9
Rodrigo Rafa <rodrigorafa71@gmail.com>
2023-09-20 10:41:05 -0300

    if_maior
    Signed-off-by: Rodrigo Rafa <rodrigorafa71@gmail.com>

--- Exemplos/repeticao2.py
+++ Exemplos/repeticao2.py
@@ -2,3 +2,8 @@ import sys
 MAIOR_IDADE = 18
 idade = int(input("Qual a sua idade?: "))
 
+if idade >= 18:
+    input("Seu cpf para cadastro:")
+    input("Digite seu email: ")
+    pagamento = int(input("Forma de pagamento da CNH: [1]Débito [2]Crédito [3]Boleto\n"))
+    
